cbc663d114154170188e0908a56d0d3430c368b9
Clement Macharia <clementmacharia62@gmail.com>
2024-02-02 15:29:39 +0300

    complete

--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+import re
+
 db = SQLAlchemy()
 
 class Author(db.Model):
@@ -12,6 +14,22 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    # All authors have a name
+    # Author phone numbers are exactly ten digits
+    @validates("name", "phone_number")
+    def validate_author(self, key, value):
+        if key == "name":
+            assert value != ""
+        if key == "phone_number":
+            assert len(value) == 10
+        return value
+   
+    
+
+      
+    
+    
+        
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +45,26 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators 
+    # All posts have a title
+    # Post content is at least 250 characters long
+    # Post summary is a maximum of 250 characters 
+    @validates("title", "content", "summary")
+    def validate_post(self, key, value):
+        if key == "title":
+            assert value != ""
+        if key == "content":
+            assert len(value) >= 250
+        if key == "summary":
+            assert len(value) <= 250
+        return value
+    
+    def validate_clickbait_title(self, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top [0-9]+", "Guess"]
+        for phrase in clickbait_phrases:
+            if re.search(phrase, title):
+                return
+        raise ValueError("Title must be sufficiently clickbait-y. It should contain phrases like 'Won't Believe', 'Secret', 'Top [number]', or 'Guess'.")
 
 
     def __repr__(self):
